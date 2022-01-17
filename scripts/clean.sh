2dbed2d5531a14bd9c536916486f76daecbfaecb
nasingh <narendra.cs.biet@gmail.com>
2022-01-17 13:11:35 +0530

    clean.sh updated to remove stopped container with webserver name

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-container_id=$(docker container ls -q -f name=webserver)
-if [ ! -z $container_id ] ; then docker container rm -f $container_id;  fi
+container_id=$(docker container ls -a -q -f name=webserver)
+if [ ! -z $container_id ] ; then docker container rm -f $container_id;echo "webserver container has been removed";  fi
 image_id=$(docker image ls|grep 'narendra3425/webserver'|tr -s ' '|cut -d' ' -f3)
 if [ ! -z $image_id ] ; then docker image rm -f $image_id;  fi
